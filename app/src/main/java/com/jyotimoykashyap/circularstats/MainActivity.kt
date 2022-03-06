5acd0d19ca45dbb78d1b5a721fac739597ca6e5d
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2022-03-06 14:29:52 +0530

    bug-fix : fixed dark mode issue

--- app/src/main/java/com/jyotimoykashyap/circularstats/MainActivity.kt
+++ app/src/main/java/com/jyotimoykashyap/circularstats/MainActivity.kt
@@ -1,18 +1,17 @@
 package com.jyotimoykashyap.circularstats
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,7 +27,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CircularStatsTheme {
-                MainView()
+                Surface {
+                    MainView()
+                }
             }
         }
     }
@@ -42,11 +43,10 @@ fun MainView(){
         verticalArrangement = Arrangement.Center
     ) {
 
-        var textState by remember{ mutableStateOf(0)}
+        var textState by remember{ mutableStateOf(10)}
         CircularStats(
             canvasSize = 200.dp,
             indicatorValue = textState,
-            progressTextColor = MaterialTheme.colors.onBackground,
             animationType = AnimateCompletion.CHECKMARK
         )
 
@@ -63,7 +63,8 @@ fun MainView(){
                             },
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Number
-            )
+            ),
+
         )
     }
 }
@@ -73,6 +74,13 @@ fun MainView(){
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
+    CircularStatsTheme {
+        MainView()
+    }
+}
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun DarkModePreview(){
     CircularStatsTheme {
         MainView()
     }
